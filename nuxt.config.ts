99b57cd5af143d9b90d25d84d0e14756dc7fb990
lulcca <lucca.jacomassi@hotmail.com>
2025-01-27 14:48:43 -0300

    :heavy_plus_sign: add vitest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxt/eslint',
+    '@nuxt/test-utils/module',
     '@nuxt/ui',
     '@nuxtjs/tailwindcss',
   ],
